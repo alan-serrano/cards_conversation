190a3258a1fa5369d606f1f238624c20466c6cb2
alan-serrano <alan.serrano.pch@gmail.com>
2020-05-09 10:25:31 -0400

    Updates

---
+++ precache-manifest.aa3d1029998c81837302f4e7480686c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5acc9d18fca1e0f21e9558702ba2b24d",
+    "url": "/cards_conversation/index.html"
+  },
+  {
+    "revision": "f1308469ffed3cb22ae6",
+    "url": "/cards_conversation/static/css/main.2e2fc665.chunk.css"
+  },
+  {
+    "revision": "a0a9694a577f7a387b02",
+    "url": "/cards_conversation/static/js/2.0aa7c899.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cards_conversation/static/js/2.0aa7c899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1308469ffed3cb22ae6",
+    "url": "/cards_conversation/static/js/main.9346757d.chunk.js"
+  },
+  {
+    "revision": "f0a6c71db30900f19574",
+    "url": "/cards_conversation/static/js/runtime-main.0d56071b.js"
+  }
+]);
