7fb74b75aebc0610bcaf7e7d4be3e006017c59d8
alan-serrano <alan.serrano.pch@gmail.com>
2020-05-20 13:41:04 -0400

    Landing view

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import './Landing.scss';
 
-function Landing() {
+/* MATERIAL UI */
+// Layout
+import Container from '@material-ui/core/Container';
+
+// Inputs
+import Button from '@material-ui/core/Button';
+
+function Landing({createNewParty, randomCard, dataIsReady}) {
+
     return (
         <div className="landing-page">
-            Página Landing
+            <Container maxWidth="xs" className="container">
+                <h1>Conversation <br/> Starters</h1>
+
+                <div className="wrapper-button">
+                    <Button size="large" disabled={!dataIsReady} fullWidth variant="contained" color="primary" onClick={createNewParty}>
+                        Create Room
+                    </Button>
+                    <Button size="large" disabled={!dataIsReady} fullWidth variant="contained" color="primary" onClick={randomCard}>
+                        Random Card
+                    </Button>
+                </div>
+            </Container>
         </div>
     );
 }
